46e860e460fe1a688d1038defbfb64dfa543424e
Hibiki Shibata <hibiki.shibata@wolt.com>
2025-05-24 02:42:11 +0900

    added flowFile model & migration succeed

--- apps/meterData_handler_app/models/models.py
+++ apps/meterData_handler_app/models/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class flowFile(models.Model):
+    file_name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 # D0010 model: D0010 data doc => https://www.electralink.co.uk/data-catalogues/dtc-catalogue/
 class MeterReading(models.Model):
@@ -8,8 +12,9 @@ class MeterReading(models.Model):
     register_id = models.CharField(max_length=2)
     reading_date = models.DateField()
     meter_reading = models.DecimalField(max_digits=10, decimal_places=1)
+    file_name = models.ForeignKey(flowFile, on_delete=models.CASCADE)
+    
+
 
 
         
-
-
