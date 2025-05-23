8cf61a2d27348c790eb48f789237f46d38afb2a2
Hibiki Shibata <hibiki.shibata@wolt.com>
2025-05-24 00:11:26 +0900

    first commit - tidy file structure & prep minimum dependencies

---
+++ apps/meterData_handler_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MeterdataHandlerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.meterData_handler_app'
---
+++ apps/meterData_handler_app/management/commands/__init__.py
@@ -0,0 +1,1 @@
+from .import_d0010 import Command
