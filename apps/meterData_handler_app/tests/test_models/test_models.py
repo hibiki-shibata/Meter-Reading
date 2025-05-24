5c54c465366eb08d0c5487d6476db164292421f4
Hibiki Shibata <hibiki.shibata@wolt.com>
2025-05-24 19:54:38 +0900

    test_models.py all edgecases covered

--- apps/meterData_handler_app/tests/test_models/test_models.py
+++ apps/meterData_handler_app/tests/test_models/test_models.py
@@ -40,9 +40,13 @@ class MeterReadingModelTest(TestCase):
         self.assertTrue(len(reading.meter_serial_number) <= 10)
 
 
-    def test_missing_required_fields(self):
+
+
+class MeterReadingMissingFieldTest(TestCase):
+
+    def test_mpam_missing_required_fields(self): # mpan is missing
         readingMissField = MeterReading(
-            meter_serial_number="A123456789",  # mpan is missing
+            meter_serial_number="A123456789",  
             register_id="01",
             reading_date=date(2024, 5, 20),
             reading_value=Decimal("1234.5"),
@@ -52,16 +56,111 @@ class MeterReadingModelTest(TestCase):
             readingMissField.full_clean() 
 
 
-    def test_too_long_mpan_fails(self):
-        readingTooLongField = MeterReading(
-            mpan="1" * 14,  # too long
-            meter_serial_number="A123456789",
+    def test_serial_missing_required_fields(self): # meter_serial_number is missing
+        readingMissField = MeterReading(
+            mpan="1234567890123",
             register_id="01",
             reading_date=date(2024, 5, 20),
             reading_value=Decimal("1234.5"),
             file_name="flow_file_001.d0010"
         )
-        
         with self.assertRaises(ValidationError):
-            readingTooLongField.full_clean()
+            readingMissField.full_clean() 
+
+
+    def test_registerid_missing_required_fields(self): # register_id is missing
+        readingMissField = MeterReading(
+            mpan="1234567890123",
+            meter_serial_number="A123456789",
+            reading_date=date(2024, 5, 20),
+            reading_value=Decimal("1234.5"),
+            file_name="flow_file_001.d0010"
+        )
+        with self.assertRaises(ValidationError):
+            readingMissField.full_clean() 
+
+
+    def test_readingDate_missing_required_fields(self): # reading_date is missing
+        readingMissField = MeterReading(
+            mpan="1234567890123",
+            meter_serial_number="A123456789",
+            register_id="01",
+            reading_value=Decimal("1234.5"),
+            file_name="flow_file_001.d0010"
+        )
+        with self.assertRaises(ValidationError):
+            readingMissField.full_clean() 
+
+
+    def test_readingValue_missing_required_fields(self): # reading_value is missing
+        readingMissField = MeterReading(
+            mpan="1234567890123",
+            meter_serial_number="A123456789",
+            register_id="01",
+            reading_date=date(2024, 5, 20),
+            file_name="flow_file_001.d0010"
+        )
+        with self.assertRaises(ValidationError):
+            readingMissField.full_clean() 
+
+
+    def test_fileName_missing_required_fields(self): # file_name is missing
+        readingMissField = MeterReading(
+            mpan="1234567890123",
+            meter_serial_number="A123456789",
+            register_id="01",
+            reading_date=date(2024, 5, 20),
+            reading_value=Decimal("1234.5"),
+        )
+        with self.assertRaises(ValidationError):
+            readingMissField.full_clean() 
+
+
+
+
+
+class MeterReadingFieldLengthTest(TestCase):
+
+    def create_valid_reading(self, **overrides):
+        data = {
+            'mpan': '1234567890123',
+            'meter_serial_number': 'ABC1234567',
+            'register_id': '01',
+            'reading_date': date(2024, 5, 24),
+            'reading_value': Decimal('1234.5'),
+            'file_name': 'test_file.d0010'
+        }
+        data.update(overrides)
+        return MeterReading(**data)
+
+    def test_mpan_too_long_raises(self):
+        reading = self.create_valid_reading(mpan='1' * 14)  # 14 > max_length 13
+        with self.assertRaises(ValidationError):
+            reading.full_clean()
+
+    def test_meter_serial_number_too_long_raises(self):
+        reading = self.create_valid_reading(meter_serial_number='X' * 11)  # max_length = 10
+        with self.assertRaises(ValidationError):
+            reading.full_clean()
+
+    def test_register_id_too_long_raises(self):
+        reading = self.create_valid_reading(register_id='XYZ')  # max_length = 2
+        with self.assertRaises(ValidationError):
+            reading.full_clean()
+
+    def test_file_name_too_long_raises(self): # file_name max_length = 100
+        long_file_name = 'f' * 101
+        reading = self.create_valid_reading(file_name=long_file_name)
+        with self.assertRaises(ValidationError):
+            reading.full_clean()
+
+    def test_invalid_date_raises(self):
+        reading = self.create_valid_reading(reading_date='not-a-date')
+        with self.assertRaises(ValidationError):
+            reading.full_clean()
+
+    def test_invalid_reading_value_type_raises(self):
+        reading = self.create_valid_reading(reading_value='not-a-decimal')
+        with self.assertRaises(ValidationError):
+            reading.full_clean()
 
