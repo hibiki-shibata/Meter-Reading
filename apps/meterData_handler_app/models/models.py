e2855043132984766948b87a7c3983979555c64f
hibiki-shibata <hibikishibata1853@gmail.com>
2025-06-03 03:11:45 +0900

    mpan & SN made  db_index

--- apps/meterData_handler_app/models/models.py
+++ apps/meterData_handler_app/models/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # D0010 model: D0010 data doc => https://www.electralink.co.uk/data-catalogues/dtc-catalogue/
 class MeterReading(models.Model):
-    mpan = models.CharField(max_length=13)
-    meter_serial_number = models.CharField(max_length=10)
+    mpan = models.CharField(max_length=13, db_index=True)
+    meter_serial_number = models.CharField(max_length=10, db_index=True)
     register_id = models.CharField(max_length=2)
     reading_date = models.DateField()
     reading_value = models.DecimalField(max_digits=10, decimal_places=1)
-    file_name = models.CharField(max_length=100)    
+    file_name = models.CharField(max_length=100) 
