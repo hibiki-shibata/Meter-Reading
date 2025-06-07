3397c64579abc09d74ec74c9c0fd011b3acd964f
hibiki-shibata <hibikishibata1853@gmail.com>
2025-06-08 05:02:32 +0900

    celery file

---
+++ server_config/celeryconfig/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from celery import Celery
+
+# Celery config: https://docs.celeryq.dev/en/latest/django/first-steps-with-django.html
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_config.celeryconfig.settings') # This path shoudld be relative path from entry point
+
+app = Celery('celeryconfig')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
