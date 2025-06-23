d65208ad67f48144028808d18941d41fc1374842
hibiki-shibata <hibikishibata1853@gmail.com>
2025-06-24 01:11:00 +0900

    view and url handler added

--- apps/meterData_handler_app/urls.py
+++ apps/meterData_handler_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.meterData_handler_app.views.views import MeterDataHandlerView
+from apps.meterData_handler_app.views.views import MeterDataHandlerView, MeterFileCreateView
 
 urlpatterns = [
     path('meterData/', MeterDataHandlerView.as_view(), name='meter_data_handler'),
+    path('meterData/create/', MeterFileCreateView.as_view(), name='meter_file_create'),
 ]
--- apps/meterData_handler_app/views/views.py
+++ apps/meterData_handler_app/views/views.py
@@ -4,8 +4,12 @@ from rest_framework import generics , status
 
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 
+from rest_framework.views import APIView
+
 from apps.meterData_handler_app.models import MeterReading
 from apps.meterData_handler_app.serializers.meterReadSerializer import reqMeterReadSerializer
+
+from apps.meterData_handler_app.tasks.file_reader import d0010_importer
 
 
 
@@ -22,9 +26,18 @@ class MeterDataHandlerView(generics.GenericAPIView, ListModelMixin, CreateModelM
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
+        
+        
+class MeterFileCreateView(APIView):
+
     def post(self, request, *args, **kwargs):
         try:
-            return self.create(request, *args, **kwargs)
-        
+            file = request.FILES.get("file")
+            if not file:
+                return Response({"error": "No file provided"}, status=status.HTTP_400_BAD_REQUEST)
+                        
+            d0010_importer(request.FILES["file"].read().decode('utf-8'), file.name)
+            return Response({"message": "File is being processed"}, status=status.HTTP_202_ACCEPTED)
+
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
