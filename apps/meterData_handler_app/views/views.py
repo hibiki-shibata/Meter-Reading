c990ad4bf1f89f7409d338ee387b2c9b4bf7ba30
hibiki-shibata <hibikishibata1853@gmail.com>
2025-06-24 02:14:34 +0900

    fixed file reader using csv in viwe

--- apps/meterData_handler_app/views/views.py
+++ apps/meterData_handler_app/views/views.py
@@ -38,14 +38,12 @@ class MeterFileCreateView(APIView):
             if not file:
                 return Response({"error": "No file provided"}, status=status.HTTP_400_BAD_REQUEST)
                         
-            if not file.name.srtendswith('d0010'):
-                return Response({"error": "Invalid file type. Only .d0010 files are allowed."}, status=status.HTTP_400_BAD_REQUEST)
-                                    
-            text_stream = TextIOWrapper(file.file, encoding='utf-8', newline='')
-            fileData = csv.reader(text_stream, delimiter='|')
+            if file.name.srtendswith('d0010'):                                    
+                text_stream = TextIOWrapper(file.file, encoding='utf-8', newline='')
+                fileData = csv.reader(text_stream, delimiter='|')
 
-            d0010_importer(fileData, file.name)
-            return Response({"message": "File is being processed"}, status=status.HTTP_202_ACCEPTED)
+                d0010_importer(fileData, file.name)
+                return Response({"message": "File is being processed"}, status=status.HTTP_202_ACCEPTED)
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
